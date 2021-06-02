d212fd2b045a4358594e247eda8e1129f6171a9a
akash-akya <akashh246@gmail.com>
2021-06-03 00:01:12 +0530

    Bump to v0.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCmd.MixProject do
   def project do
     [
       app: :ex_cmd,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
